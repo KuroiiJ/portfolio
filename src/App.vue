9507f50d510cbfc3d73ec94ff43f714a0f54eb6a
ChloeJ <chloe.jandsten@gmail.com>
2020-05-07 20:08:26 -0400

    Portfolio: Vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,100 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Users />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Users from './components/Users.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Users
   }
 }
 </script>
 
-<style>
+<style lang="sass">
+@import "~bulma/bulma.sass"
+</style>
+
+
+
+<template>
+  <div id="app">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <router-link class="navbar-item" to="/">
+          <img src="./assets/chloe-logo.png" height="75">
+        </router-link>
+
+        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" :class="{ 'is-active': showNav }" @click="showNav = !showNav">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarBasicExample" class="navbar-menu" :class="{ 'is-active': showNav }">
+        <div class="navbar-start">
+
+        </div>
+
+        <div class="navbar-end">
+          <router-link to="/" class="navbar-item">
+            Home
+          </router-link>
+          <router-link to="/about" class="navbar-item">
+           About
+          </router-link>
+          <router-link to="/projects" class="navbar-item">
+           Projects
+          </router-link>
+          <router-link to="/contact" class="navbar-item">
+           Contact
+          </router-link>
+       </div>
+     </div>
+   </nav>
+   <router-view />
+   <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        Built by Chloe Jandsten with Vue.js, Bulma, and Airtable.
+      </p>
+    </div>
+  </footer>
+</div>
+</template>
+
+<script>
+  export default {
+    name: "App",
+    data() {
+      return{
+        showNav: false
+      }
+    },
+  };
+</script>
+
+<style type="text/css">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  min-height: 100vh;
+  overflow: hidden;
+  display: block;
+  position: relative;
+  padding-bottom: 168px; /* height of your footer */
 }
+footer {
+ position: absolute;
+ bottom: 0;
+ width: 100%;
+}
+</style>
+
+<style lang="sass">
+@import "~bulma/bulma.sass"
 </style>
