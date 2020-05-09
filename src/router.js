79b0a7e5e809087926aa034ee7a02d38522cc229
ChloeJ <chloe.jandsten@gmail.com>
2020-05-09 17:27:16 -0400

    Heroku config

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import About from "./views/About.vue";
 import Contact from "./views/Contact.vue";
 import Projects from "./views/Projects.vue"
 
@@ -19,6 +20,11 @@ export default new Router({
       path: "/contact",
       name: "contact",
       component: Contact
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: About
     },
     {
       path: "/projects",
---
+++ src/views/About.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <section class="hero is-medium is-info is-bold">
+			<div class="hero-body">
+				<div class="container">
+					<h1 class="title is-2">
+						Highlights About... Me!
+					</h1>
+				</div>
+			</div>
+		</section>
+		<section class="section">
+			<div class="container is-fluid">
+				<div class="columns is-multiline">
+						<div class="column is-one-third" v-for="project in projects" v-bind:key="project.slug">
+							<post-card v-bind="project"></post-card>
+						</div>				
+				</div>
+			</div>
+		</section>
+        <section>
+            <content class="is-large">
+                <h1>Hi</h1>
+            </content>
+        </section>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "about",
+};
+
+</script>
