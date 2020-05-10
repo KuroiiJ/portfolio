7c3e6cc4bae77ca2932bb2c715b3f15c8ccd3c13
ChloeJ <chloe.jandsten@gmail.com>
2020-05-10 14:55:00 -0400

    Added Tech

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,18 +3,37 @@
     <div class="hero is-cover is-relative is-fullheight-with-navbar is-bold is-info">
         <div class="hero-body">
             <div class="container">
-                <div class="columns">
+                <div class="columns is-vcentered">
                     <div class="column is-one-third">
-                        <figure class="image is-300x300">
+                        <figure class="image is-256x256">
                           <img class="is-rounded" src="../assets/c-and-h.jpg">
                         </figure>
                     </div>
                     <div class="column">
-                        <h1 class="title is-2"> <span class="multicolortext"><br/><br/>I'm Chloé Jandsten</span></h1>
+                        <h1 class="title is-2"> <span class="multicolortext">I'm Chloé Jandsten</span></h1>
                         <br />
                         <h3 class="subtitle is-4">I am a NYC-based fullstack software engineer with a background in developer marketing, account management, and start-ups.</h3>
+                        <br />
+                        <h3 class="subtitle is-4">Some tech I've been into:</h3>
+                        <div class="tags are-medium">
+                          <span class="tag is-primary is-light">Javascript</span>
+                          <span class="tag is-link is-light">Node.js</span>
+                          <span class="tag is-info is-light">React</span>
+                          <span class="tag is-success is-light">Redux</span>
+                          <span class="tag is-warning is-light">CSS/HTML</span>
+                          <span class="tag is-info is-light">VueJS</span>
+                          <span class="tag is-warning is-light">PostgreSQL</span>
+                          <span class="tag is-link is-light">MongoDB</span>
+                          <span class="tag is-primary is-light">Airtable</span>
+                          <span class="tag is-info is-light">PUG</span>
+                          <span class="tag is-warning is-light">Express</span>
+                          <span class="tag is-link is-light">PWA's</span>
+                          <span class="tag is-success is-light">Electron</span>
+
+                          
+                        </div>
                     </div>
-                    </div>
+                </div>
                 
             </div>
         </div>
