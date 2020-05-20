331f21cd0df5319d63d6892ff3da95f159626477
ChloeJ <chloe.jandsten@gmail.com>
2020-05-19 21:49:31 -0400

    pic

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -40,31 +40,38 @@
 					<div class="columns is-centered">
 						<div class="column is-three-fifths">
 							<div class="box">		
-				<p class="buttons is-centered">
-					<button class="button">
+								<p class="buttons is-centered">
+									<button class="button">
 
-					<span><a href="https://github.com/KuroiiJ"><font-awesome-icon :icon="['fab', 'github']" />  GitHub </a></span>
-					</button>
-					<button class="button">
-					<span><a href="https://twitter.com/captainkuroii"> <font-awesome-icon :icon="['fab', 'twitter']" /> Twitter</a></span>
-					</button>
-					<button class="button">
-					<span><a href="mailto:chloe.jandsten@gmail.com"> <font-awesome-icon :icon="['fas', 'paper-plane']" /> Email Me</a></span>
-					</button>
-					<button class="button">
-					<span><a href="https://www.linkedin.com/in/chloejandsten/"> <font-awesome-icon :icon="['fab', 'linkedin']" /> LinkedIn</a></span>
-					</button>
-					<button class="button">
-					<span><a href="https://www.instagram.com/hondo_the_doggo/"> <font-awesome-icon :icon="['fab', 'instagram']" /> Dog Instagram</a></span>
-					</button>
-				</p>
-				</div>
+									<span><a href="https://github.com/KuroiiJ"><font-awesome-icon :icon="['fab', 'github']" />  GitHub </a></span>
+									</button>
+									<button class="button">
+									<span><a href="https://twitter.com/captainkuroii"> <font-awesome-icon :icon="['fab', 'twitter']" /> Twitter</a></span>
+									</button>
+									<button class="button">
+									<span><a href="mailto:chloe.jandsten@gmail.com"> <font-awesome-icon :icon="['fas', 'paper-plane']" /> Email Me</a></span>
+									</button>
+									<button class="button">
+									<span><a href="https://www.linkedin.com/in/chloejandsten/"> <font-awesome-icon :icon="['fab', 'linkedin']" /> LinkedIn</a></span>
+									</button>
+									<button class="button">
+									<span><a href="https://www.instagram.com/hondo_the_doggo/"> <font-awesome-icon :icon="['fab', 'instagram']" /> Dog Instagram</a></span>
+									</button>
+								</p>
+							</div>
 						</div>
 					</div>
 				</div>
+				<br /><br /><br /><br />
+				<div class="columns is-centered">
+                    <div class="column is-one-third">
+						<figure class="image is-256x256">
+							<img class="is-5-by-4" src="../assets/meta-image.png">
+						</figure>
+					</div>
+                 </div>
 			</container>
 		</section>			
-
 
 	</div>
 </template>
